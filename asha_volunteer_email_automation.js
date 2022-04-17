d0dbc00613433d0f00b6aa02e015483f3ce19274
zachsnyder1 <zachsnyder3@gmail.com>
2022-04-17 08:30:38 -0700

    refactoring to class structure

--- asha_volunteer_email_automation.js
+++ asha_volunteer_email_automation.js
@@ -1,101 +1,168 @@
-// extraction dictionary key constants - same as template tags
-const KEY_ROLE = "||VOLUNTEER ROLE||";
-const KEY_NAME = "||VOLUNTEER NAME||";
-const KEY_EMAIL = "||VOLUNTEER EMAIL||";
-// -----------  TEST INFO  -----------
-// -----------------------------------
 const test_params = DocumentApp.openById("1iqCTejm6Nb1L-WzUyM30n9zlPYBpQKEEhSscQpmnnbY").
                               getBody().getText();
-const email_pattern = /SENDEREMAIL:(.*)\n/g;
-const template_pattern = /TEMPLATEID:(.*)\n/g;
+var email_pattern = /SENDEREMAIL:(.*)\n/g;
+var template_pattern = /TEMPLATEID:(.*)\n/g;
+var outdoc_pattern = /OUTDOC:(.*)\n/g;
 const TEMPLATE_ID_TEST = template_pattern.exec(test_params)[1];
 const SENDER_TEST = email_pattern.exec(test_params)[1];
-var matches_test = [];
-// ---------  END TEST INFO  ---------
-// -----------------------------------
-// template doc id numbers
-const TEMPLATE_ID_VOLMATCH = null;
-const TEMPLATE_ID_BENEVITY = null;
-const TEMPLATE_ID_IDEALIST = null;
-const TEMPLATE_ID_GIFTPULSE = null;
-// subject search string constants
-const SUBJ_SEARCH_VOLMATCH = "Someone wants to help: ";
-const SUBJ_SEARCH_BENEVITY = "";
-const SUBJ_SEARCH_IDEALIST = "";
-const SUBJ_SEARCH_GIFTPULSE = "";
-// sender search string constants
-const SENDER_VOLMATCH = "noreply@volunteermatch.org";
-const SENDER_BENEVITY = "";
-const SENDER_IDEALIST = "";
-const SENDER_GIFTPULSE = "";
-// extraction dictionary constants - used to find role, name, and email based on
-// what substrings bracket the desired string (i.e. come before and after it)
-const EXTR_VOLMATCH = {};
-EXTR_VOLMATCH[KEY_ROLE] = /Title: (.*)\n/g;
-EXTR_VOLMATCH[KEY_NAME] = /Name: (.*)\n/g;
-EXTR_VOLMATCH[KEY_EMAIL] = /Email: (.*)\n/g;
-// for storing emails matching each source
-var matches_VolMatch = [];
-var matches_Benevity = [];
-var matches_Idealist = [];
-var matches_GiftPulse = [];
+const OUTDOC_TEST = outdoc_pattern.exec(test_params)[1];
 
-// get all new volunteer notification emails from inbox, and
-// save the ones that haven't been responded to yet
-function getNewMatches(match_array, subject_search_str, sender_search_str) {
-  var threads = GmailApp.search("from:" + sender_search_str +
-                                         " subject:\"" + subject_search_str + "\"" +
-                                         " is:unread");
-  for (t = 0; t < threads.length; t++) {
-    match_array.push(threads[t]);
+/**
+ * Abstract Class VolunteerSite.
+ *
+ * @class VolunteerSite
+ */
+class VolunteerSite {
+  // set template tag constants (also used in extraction dictionaries as keys)
+  constructor() {
+    this.key_role = "||VOLUNTEER ROLE||";
+    this.key_name = "||VOLUNTEER NAME||";
+    this.key_email = "||VOLUNTEER EMAIL||";
+    this.extraction_dict = {};
+    this.values_dict = {};
+    this.subj_search = null;
+    this.sender_search = null;
+    this.matches = [];
+    this.template_doc_id = null;
+    this.site_name = null;
   }
-}
-
-// SUBROUTINE: Given a message and an extraction dictionary, return a
-// dictionary of values w/ same keys as the extraction dictionary
-function extractFromMessage(message, extr_dict) {
-  var values_dict = {};
-  var keys = Object.keys(extr_dict);
-  for (k = 0; k < keys.length; k++) {
-    // find and return a value from the message, based on
-    // the substrings that bracket the value
-    var search_pattern = extr_dict[keys[k]];
-    values_dict[keys[k]] = search_pattern.exec(message.getPlainBody())[1];
-  }
-  return values_dict;
-}
-
-// Auto-reply to emails, given an array of message objs, a
-// template id, and an extraction dictionary
-function autoReply(matches, template_doc_id, extr_dict) {
-  // retrieve the template doc based on its ID
-  var template = DocumentApp.openById(template_doc_id);
-  // for each message, perform all replacements in replacement dict
-  for (m = 0; m < matches.length; m++) {
-    var message_to_reply_to = matches[m].getMessages()[0];
-    var volunteer_info = extractFromMessage(message_to_reply_to, extr_dict);
-    // load the template text
-    var response_body = template.getBody().getText();
-    // perform replacement of the template tags
-    var keys = Object.keys(extr_dict);
-    for (k = 0; k < keys.length; k++) {
-      response_body = response_body.replaceAll(keys[k], volunteer_info[keys[k]]);
+  // METHOD: get new email matches, based on sender and subject
+  getNewMatches() {
+    var searchops = "from:" + this.sender_search +
+                                           " subject:\"" + this.subj_search + "\"" +
+                                           " is:unread";
+    var threads = GmailApp.search(searchops);
+    var t = 0;
+    for (t = 0; t < threads.length; t++) {
+      this.matches.push(threads[t]);
     }
-    var auto_email_to_volunteer = GmailApp.createDraft(volunteer_info[KEY_EMAIL],
-                                                       volunteer_info[KEY_NAME],
-                                                       response_body);
-    auto_email_to_volunteer.send();
-    message_to_reply_to.star();
-    message_to_reply_to.markRead();
+  }
+  // METHOD: Given a message, extract values
+  extractFromMessage(message) {
+    var keys = Object.keys(this.extraction_dict);
+    var k = 0;
+    for (k = 0; k < keys.length; k++) {
+      // find and return a value from the message, based on
+      // the substrings that bracket the value
+      var search_pattern = this.extraction_dict[keys[k]];
+      this.values_dict[keys[k]] = search_pattern.exec(message.getPlainBody())[1];
+    }
+  }
+}
+
+/**
+ * Concrete Class VolunteerSiteNoSeparateAuth.
+ *
+ * @class VolunteerSiteNoSeparateAuth
+ */
+class VolunteerSiteNoSeparateAuth extends VolunteerSite {
+  constructor() {
+    super();
+  }
+  // METHOD: Auto-reply to matched messages
+  autoReply() {
+    // retrieve the template doc based on its ID
+    var template = DocumentApp.openById(this.template_doc_id);
+    // for each message, perform all replacements in replacement dict
+    var m = 0;
+    for (m = 0; m < this.matches.length; m++) {
+      var message = this.matches[m].getMessages()[0];
+      this.extractFromMessage(message);
+      // load the template text
+      var response_body = template.getBody().getText();
+      // perform replacement of the template tags
+      var keys = Object.keys(this.extraction_dict);
+      var k = 0;
+      for (k = 0; k < keys.length; k++) {
+        response_body = response_body.replaceAll(keys[k], this.values_dict[keys[k]]);
+      }
+      var auto_email_to_volunteer = GmailApp.createDraft(this.values_dict[this.key_email],
+                                                         this.values_dict[this.key_name],
+                                                         response_body);
+      auto_email_to_volunteer.send();
+      message.star();
+      message.markRead();
+    }
+  }
+}
+
+/**
+ * Concrete Class VolunteerSiteSeparateAuth.
+ *
+ * @class VolunteerSiteSeparateAuth
+ */
+class VolunteerSiteSeparateAuth extends VolunteerSite {
+  constructor() {
+    super();
+    this.out_doc_id = null;
+  }
+  // METHOD: output to Google Doc, for Selenium access
+  outputToDoc() {
+    // open output doc
+    var outdoc_body = DocumentApp.openById(this.out_doc_id).getBody();
+    var updated_body_text = outdoc_body.getText();
+    // for each message, append volunteer info to the outdoc
+    var m = 0;
+    for (m = 0; m < this.matches.length; m++) {
+      var message = this.matches[m].getMessages()[0];
+      this.extractFromMessage(message);
+      var keys = Object.keys(this.extraction_dict);
+      var k = 0;
+      for (k = 0; k < keys.length; k++) {
+        updated_body_text += this.site_name + m.toString() + 
+                             "::>" + this.values_dict[keys[k]] + "<::\n";
+      message.star();
+      message.markRead();
+      }
+    }
+    outdoc_body.setText(updated_body_text);
+  }
+}
+
+/**
+ * Concrete Class Idealist.
+ *
+ * @class Idealist
+ */
+class Idealist extends VolunteerSiteSeparateAuth {
+  constructor(template_doc_id, out_doc_id) {
+    super();
+    this.subj_search = "You have a new applicant to review on Idealist!";
+    this.sender_search = SENDER_TEST //"support@idealist.org";
+    this.extraction_dict[this.key_role] = /You can log in to see their information here:\n>\n> (.*)\n/g;
+    this.extraction_dict[this.key_name] = /> \[(.*)\]\(/i;
+    this.template_doc_id = template_doc_id;
+    this.out_doc_id = out_doc_id;
+    this.site_name = "Idealist";
+  }
+}
+
+/**
+ * Concrete Class VolunteerMatch.
+ *
+ * @class VolunteerMatch
+ */
+class VolunteerMatch extends VolunteerSiteNoSeparateAuth {
+  constructor(template_doc_id, out_doc_id) {
+    super();
+    this.subj_search = "Someone wants to help: ";
+    this.sender_search = SENDER_TEST //"noreply@volunteermatch.org";
+    this.extraction_dict[this.key_role] = /Title: (.*)\n/g;
+    this.extraction_dict[this.key_name] = /Name: (.*)\n/g;
+    this.extraction_dict[this.key_email] = /Email: (.*)\n/g;
+    this.template_doc_id = template_doc_id;
+    this.out_doc_id = out_doc_id;
   }
 }
 
 // TEST
-function test() {
-  // get new Volunteer Match new volunteer notifications
-  getNewMatches(matches_test, SUBJ_SEARCH_VOLMATCH, SENDER_TEST);
-  // auto-reply to Volunteer Match new volunteer notifications
-  autoReply(matches_test, TEMPLATE_ID_TEST, EXTR_VOLMATCH);
+function doGet() {
+  var ideal = new Idealist(TEMPLATE_ID_TEST, OUTDOC_TEST);
+  ideal.getNewMatches();
+  ideal.outputToDoc();
+  var volmatch = new VolunteerMatch(TEMPLATE_ID_TEST, OUTDOC_TEST);
+  volmatch.getNewMatches();
+  volmatch.autoReply();
 }
 
-test();
+doGet();
