1eca3e574803509e0cbe83eda6c77c7896910cd4
zachsnyder1 <zachsnyder3@gmail.com>
2022-05-08 20:12:23 -0700

    moving params to .env

--- asha_volunteer_auto_main.py
+++ asha_volunteer_auto_main.py
@@ -1,4 +1,4 @@
-import os, time, re
+import os, time, re, logging
 import urllib.parse as parse
 from dotenv import load_dotenv # credentials stored in .env (git ignored)
 from selenium import webdriver
@@ -8,7 +8,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-WEBAPP_MAIN_URL = "https://script.google.com/macros/s/AKfycbw57Zc938QBa5t0hbyGm4XJp04kPRco9bZ09Dzf3vGGrrpPJleYnBOTTlqlbi67LJ9hSg/exec"
+# Parameters set in '.env' file in same directory
+GMAIL_UN = 'gmailUserName'
+GMAIL_PW = 'gmailPassword'
+IDEAL_UN = 'idealistUserName'
+IDEAL_PW = 'idealistPassword'
+WEBAPP_MAIN_URL = 'webAppUrl'
+GIVEP_UN = 'givepulseUserName'
+GIVEP_PW = 'givepulsePassword'
+# Other global constants
 IMPLICIT_WAIT_DURATION = 10
 EXPLICIT_WAIT_DURATION = 5
 SHORT_EXPLICIT_WAIT = 2
@@ -23,18 +31,31 @@ class NewVolSignup:
     self.name = name
     self.email = None
 
-def gmail_login(driver, gmail_un, gmail_pw):
+def get_env():
+  load_dotenv()
+  env = {}
+  env[GMAIL_UN] = os.environ.get(GMAIL_UN)
+  env[GMAIL_PW] = os.environ.get(GMAIL_PW)
+  env[IDEAL_UN] = os.environ.get(IDEAL_UN)
+  env[IDEAL_PW] = os.environ.get(IDEAL_PW)
+  env[GIVEP_UN] = os.environ.get(GIVEP_UN)
+  env[GIVEP_PW] = os.environ.get(GIVEP_PW)
+  env[WEBAPP_MAIN_URL] = os.environ.get(WEBAPP_MAIN_URL)
+  return env
+
+
+def gmail_login(driver, username, password):
   # First, enter username
   elem = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
     EC.presence_of_element_located((By.ID, "identifierId"))
   )
-  elem.send_keys(gmail_un + Keys.ENTER)
+  elem.send_keys(username + Keys.ENTER)
   # Next, enter password
   time.sleep(EXPLICIT_WAIT_DURATION)
   elem = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
     EC.presence_of_element_located((By.CLASS_NAME, "zHQkBf"))
   )
-  elem.send_keys(gmail_pw + Keys.ENTER)
+  elem.send_keys(password + Keys.ENTER)
 
 def find_script_complete(driver):
   sandboxFrame = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
@@ -61,20 +82,20 @@ def idealist_click_manage_listings(driver):
     if "Hi, " in menu.text:
       menu.click()
       break
-  time.sleep(EXPLICIT_WAIT_DURATION)
+  time.sleep(SHORT_EXPLICIT_WAIT)
   # locate the Asha Home Amanaki organization link
   dropdown_links = driver.find_elements(By.XPATH, "//a[contains(@class, 'PageHeaderDropdownMenu__NavLink-sc-zdvi5-0-g')]")
   for link in dropdown_links:
     if "Asha Hope Amanaki" in link.text:
       link.click()
       break
-  time.sleep(EXPLICIT_WAIT_DURATION)
+  time.sleep(SHORT_EXPLICIT_WAIT)
   # click Manage Listings on side bar
   sidebar_links = driver.find_elements(By.CLASS_NAME, "DashboardSidebarSection__StyledSidebarLink-sc-1b2a90m-1")
   for link in sidebar_links:
     if "Manage Listings" in link.text:
       link.click()
-      time.sleep(EXPLICIT_WAIT_DURATION)
+      time.sleep(SHORT_EXPLICIT_WAIT)
       
 def click_give_pulse_manage_registrations(driver):
   time.sleep(SHORT_EXPLICIT_WAIT)
@@ -94,7 +115,7 @@ def click_give_pulse_manage_registrations(driver):
 
 def idealist_admin_search(driver, search_str):
   # perform admin search for volunteer role
-  print("SEARCHING ON IDEALIST: " + search_str)
+  logging.info("SEARCHING ON IDEALIST: " + search_str)
   admin_search = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
     EC.presence_of_element_located((By.CLASS_NAME, "AdminDashboardSearchInputs__SearchQueryInput-sc-kgj4mt-2"))
   )
@@ -108,43 +129,36 @@ def is_valid_email(email_str):
   return re.fullmatch(email_regex, email_str)
 
 def main():
-  load_dotenv()
-  gmail_un = os.environ.get('gmailUserName')
-  gmail_pw = os.environ.get('gmailPassword')
-  ideal_un = os.environ.get('idealistUserName')
-  ideal_pw = os.environ.get('idealistPassword')
-  givep_un = os.environ.get('givepulseUserName')
-  givep_pw = os.environ.get('givepulsePassword')
-  
+  env = get_env()
   driver = webdriver.Firefox() # init Selenium
   # ----- VOLUNTEER MATCH -----
   # Send automatic replies to new volunteer signups on
   # volunteer accounts that provide the email in their notification
-  driver.get(WEBAPP_MAIN_URL + "?func=AutoReplyNoSeparateAuth")
+  driver.get(env[WEBAPP_MAIN_URL] + "?func=AutoReplyNoSeparateAuth")
   try:
     find_script_complete(driver)
-    print("SCRIPT COMPLETE LOCATED")
+    logging.info("SCRIPT COMPLETE LOCATED")
   except:
-    print("LOGGING IN TO GMAIL")
+    logging.info("LOGGING IN TO GMAIL")
     # if Gmail requires auth, handle that
-    gmail_login(driver, gmail_un, gmail_pw)
+    gmail_login(driver, env[GMAIL_UN], env[GMAIL_PW])
     # Finally, verify that the AutoReplyNoSeparateAuth func completed
     find_script_complete(driver)
   finally:
-    print("AutoReplyNoSeparateAuth func completed") 
+    logging.info("AutoReplyNoSeparateAuth func completed") 
   # ----- IDEALIST & GIVEPULSE -----
   # Next, run AutoReplySeparateAuth func
-  driver.get(WEBAPP_MAIN_URL + "?func=AutoReplySeparateAuth")
+  driver.get(env[WEBAPP_MAIN_URL] + "?func=AutoReplySeparateAuth")
   try:
     find_script_complete(driver)
   except:
-    print("SCRIPT COMPLETE NOT LOCATED")
+    logging.info("SCRIPT COMPLETE NOT LOCATED")
     # if Gmail requires auth, handle that
-    gmail_login(driver, gmail_un, gmail_pw)
+    gmail_login(driver, env[GMAIL_UN], env[GMAIL_PW])
     # Finally, verify that the AutoReplyNoSeparateAuth func completed
     find_script_complete(driver)
   finally:
-    print("AutoReplySeparateAuth func completed")
+    logging.info("AutoReplySeparateAuth func completed")
   # AutoReplySeparateAuth - retrieve all new signups
   new_signups_matches = driver.find_elements(By.CLASS_NAME, "volunteer_item")
   idealist_signups = []
@@ -167,13 +181,13 @@ def main():
   # login to Idealist
   login_btn.click()
   login_email = driver.find_element(By.ID, "login-form-email")
-  login_email.send_keys(ideal_un + Keys.ENTER)
+  login_email.send_keys(env[IDEAL_UN] + Keys.ENTER)
   time.sleep(EXPLICIT_WAIT_DURATION)
   login_pw = driver.find_element(By.ID, "login-form-password")
-  login_pw.send_keys(ideal_pw + Keys.ENTER)
+  login_pw.send_keys(env[IDEAL_PW] + Keys.ENTER)
   time.sleep(EXPLICIT_WAIT_DURATION)
   # for each idealist signup, gather email
-  print(str(len(idealist_signups)) + " Idealist signups")
+  logging.info(str(len(idealist_signups)) + " Idealist signups")
   for signup in idealist_signups:
     idealist_click_manage_listings(driver)
     idealist_admin_search(driver, signup.role)
@@ -183,7 +197,7 @@ def main():
     )
     listing_tiles = driver.find_elements(By.CLASS_NAME, "SearchTile-sc-1kv2zmo-2")
     listing_tiles[0].click() # assumes most recent listing!
-    time.sleep(EXPLICIT_WAIT_DURATION)
+    time.sleep(SHORT_EXPLICIT_WAIT)
     # in listing, find applicant, click
     applicant_search = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
       EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search for applicant']"))
@@ -207,14 +221,14 @@ def main():
       else:
         pass
     if signup.email:
-      print("Email found for " + signup.name + ": " + signup.email)
+      logging.info("Email found for " + signup.name + ": " + signup.email)
       signup_email_found = True
     else:
-      print("ERROR: APPLICANT NOT FOUND --> " + signup.name)
+      logging.info("ERROR: APPLICANT NOT FOUND --> " + signup.name)
   # ----- IDEALIST -----
   # AutoReplySeparateAuth - auto respond Idealist signups
   for signup in idealist_signups:
-    safe_url = WEBAPP_MAIN_URL + "?func=SendAutoReply&site=Idealist&name=" + \
+    safe_url = env[WEBAPP_MAIN_URL] + "?func=SendAutoReply&site=Idealist&name=" + \
                             parse.quote_plus(signup.name) + "&role=" + \
                             parse.quote_plus(signup.role) + \
                             "&email=" + parse.quote_plus(signup.email)
@@ -235,15 +249,15 @@ def main():
   email_input = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
     EC.presence_of_element_located((By.ID, "LoginForm_email"))
   )
-  email_input.send_keys(givep_un)
+  email_input.send_keys(env[GIVEP_UN])
   password_input = WebDriverWait(driver, IMPLICIT_WAIT_DURATION).until(
     EC.presence_of_element_located((By.ID, "LoginForm_password"))
   )
-  password_input.send_keys(givep_pw + Keys.ENTER)
+  password_input.send_keys(env[GIVEP_PW] + Keys.ENTER)
   # Go to registrations page
   click_give_pulse_manage_registrations(driver)
   # For each new volunteer, find email
-  print(str(len(givepulse_signups)) + " GivePulse signups")                             
+  logging.info(str(len(givepulse_signups)) + " GivePulse signups")                             
   for signup in givepulse_signups:
     # refresh manage registrations page
     click_give_pulse_manage_registrations(driver)
@@ -269,13 +283,12 @@ def main():
   # ----- GIVEPULSE -----
   # AutoReplySeparateAuth - auto respond GivePulse signups
   for signup in givepulse_signups:
-    safe_url = WEBAPP_MAIN_URL + "?func=SendAutoReply&site=Givepulse&name=" + \
+    safe_url = env[WEBAPP_MAIN_URL] + "?func=SendAutoReply&site=Givepulse&name=" + \
                             parse.quote_plus(signup.name) + "&role=" + \
                             parse.quote_plus(signup.role) + \
                             "&email=" + parse.quote_plus(signup.email)
     driver.get(safe_url)
     find_script_complete(driver)
-  
   # END SESSION
   driver.close()
   
